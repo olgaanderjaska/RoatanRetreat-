3afd1aae2d18b0473205d3d2beeac8a91813c2bc
Olga Anderjaska <olgaanderjaska@yahoo.com>
2017-11-19 13:26:08 -0500

    Final

---
+++ add.js
@@ -0,0 +1,13 @@
+var submitBtn = document.getElementById("submitBtn");
+console.log(submitBtn);
+
+submitBtn.addEventListener("click", function(){
+      alert("Your dates were successfully submited!");
+ })
+ var target = (" ");
+
+function subForm() {
+	return(" ");
+}
+$("#subBtn").empty();
+console.log(" ");
