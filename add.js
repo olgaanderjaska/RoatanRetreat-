3705f767d0aaff4c142083c0f35b04f5c8f76697
Olga Anderjaska <olgaanderjaska@yahoo.com>
2017-12-04 18:22:40 -0500

    some changes

--- add.js
+++ add.js
@@ -1,13 +1,29 @@
-var submitBtn = document.getElementById("submitBtn");
-console.log(submitBtn);
 
-submitBtn.addEventListener("click", function(){
-      alert("Your dates were successfully submited!");
- })
-//  var target = (" ");
+ //Validation fails if the input is blank
 
-// function subForm() {
-// 	return(" ");
-// }
-// $("#subBtn").empty();
-// console.log(" ");
+var subBtn = document.getElementById("subBtn");
+
+subBtn.addEventListener("click", validate)
+
+function validate(){
+	var errors = "";
+	errors+= allValidation();
+	console.log(errors)
+	
+	if (errors.length > 0){
+		alert(errors);
+	}
+}
+
+ function allValidation(){
+	var checkIn = document.getElementById("checkIn").value;
+	var checkOut = document.getElementById("checkOut").value;
+	var adults = document.getElementById("num1").value;
+	var kids = document.getElementById("num2").value;
+	
+	 if (checkIn === "" || checkOut === "" || adults === "" || kids === "") {
+	 	return("\nInput is empty!");
+	 }else{
+	 	return("Your dates were successfully submited!") 
+   }
+}
